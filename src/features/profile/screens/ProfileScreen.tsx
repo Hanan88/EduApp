13b192bc0391d23984656725ac92204df098ec30
Hanan Elhariri <hanan.elhariri@yourparts.com>
2024-11-20 12:12:20 +0200

    feat: add ProfileScreen

---
+++ src/features/profile/screens/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import { Button, View } from 'react-native';
+ 
+export default function ProfileScreen() {
+    // ----------------------------------------------------------------------------------------------------
+    // MARK: Main Component UI
+    // ----------------------------------------------------------------------------------------------------
+    return (
+        <View>
+            <Button title='Logout'  onPress={() => {}} />
+        </View>
+    );
+}
